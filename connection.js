2f78e62ede5214dfc09cef5ab77db85b6112a88a
Hieu-Hieu <your-public-email>
2022-05-25 17:50:15 +0700

    Added a Procfile.

--- connection.js
+++ connection.js
@@ -2,22 +2,22 @@ import { Client } from '@elastic/elasticsearch';
 import dotenv from 'dotenv';
 dotenv.config();
 
-var client = new Client({
-    node: `http://${process.env.ES_USERNAME}:${process.env.ES_PASSWORD}@localhost:9200/`
+// var client = new Client({
+//     node: `http://${process.env.ES_USERNAME}:${process.env.ES_PASSWORD}@localhost:9200/`
 
-});
-
-// const client = new Client({
-//     node: "http://34.202.227.22:9200",
-//     auth: {
-//         username: "phunghx",
-//         password: 'Tu240900',
-//     },
-//     tls: {
-//         // might be required if it's a self-signed certificate
-//         rejectUnauthorized: false,
-//     },
-//     requestTimeout: 9999999,
 // });
+
+const client = new Client({
+    node: "https://34.202.227.22:9200",
+    auth: {
+        username: "phunghx",
+        password: 'phunghx',
+    },
+    tls: {
+        // might be required if it's a self-signed certificate
+        rejectUnauthorized: false,
+    },
+    requestTimeout: 9999999,
+});
 
 export default client; 
